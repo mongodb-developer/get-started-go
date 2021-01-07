8866b856430256bd76dfbbb378337b19758638a1
Wan Bachtiar <sindbach@gmail.com>
2021-01-08 10:39:56 +1100

    Updated version substitution regex

--- get-started.sh
+++ get-started.sh
@@ -10,5 +10,5 @@ echo "Executing ... "
 docker run --rm -e MONGODB_URI=${MONGODB_URI} \
     -v "$(pwd)":/workspace \
     -w /workspace/go start-go \
-    "sed -i s/v[x0-9]\.[x0-9]+\.[x0-9]+/v${DRIVER_VERSION}/g /workspace/go/go.mod; \
+    "sed -i 's/mongo-driver v[x0-9]\+\.[x0-9]\+\.[x0-9]\+/mongo-driver v${DRIVER_VERSION}/g' /workspace/go/go.mod; \
     go run getstarted.go"
